368a2c01b41eeef808125ceac768af18dcd5bb9c
JesusNavaz <O.Schiller@htlvb.at>
2025-09-16 12:28:46 +0200

    add_something2 #2

--- Program.cs
+++ Program.cs
@@ -4,3 +4,4 @@ Console.WriteLine("Version #3");
 Console.WriteLine("add_something #1");
 Console.WriteLine("add_something #2");
 Console.WriteLine("add_something2 #1");
+Console.WriteLine("add_something2 #2");
